4a7d1d52eb06e804b2f1cb819d384fcf3d248464
FiKli-ctrl <fionaklimecki1@gmail.com>
2025-02-18 17:43:15 +0100

    Added AI API Integration

--- src/index.js
+++ src/index.js
@@ -1,12 +1,32 @@
-function generateTip(event) {
-    event.preventDefault();
+function displayTip(response) {
 
-    new Typewriter("#tip", { 
-        strings: "Prioritize your sleep and go to bed early.",
+    console.log("tip generated");
+  new Typewriter("#tip", { 
+        strings: response.data.answer,
         autoStart: true,
         delay: 1,
         cursor: "",
     });
+}
+
+
+function generateTip(event) {
+    event.preventDefault();
+
+    let instructionsInput = document.querySelector("#instructions");
+    let apiKey = "b9d4b0f1302d1ta5ba14cob9718ff6e7";
+    let prompt = `Generate a short health and wellbeing tip regarding ${instructionsInput.value}`;
+    let context = "You are a helpful assistant who is passionate about people's wellbeing";
+    let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+    console.log("generating tip");
+    console.log(`Prompt: ${prompt}`);
+    console.log(`Context; ${context}`);
+    
+    axios.get(apiURL).then(displayTip);
+
+
+  
 
 
 }
